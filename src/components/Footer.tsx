61d260c7a727284bd7a45efe0d5bc6291c9ddddc
Eleazer Quayson <117750901+thatfordjourboy@users.noreply.github.com>
2025-03-01 22:14:01 +0000

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 // Simple component for social icons
 const SocialIcon = ({ href, icon }: { href: string; icon: string }) => {
